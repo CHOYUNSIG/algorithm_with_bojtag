2b0d8d29428530d88c225b88a90a585baf3aa33c
조윤식 <cho.yunsig56@gmail.com>
2024-06-23 03:11:10 +0900

    csvLoader 형식 수정

--- src/layout/Tags.js
+++ src/layout/Tags.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+import csvLoader from "../util/csvLoader";
+
 export default function Tags() {
-        
+    const [tags, setTags] = useState(null);
+    const [related, setRelated] = useState(null);
+    const [group, setGroup] = useState(null);
+    const [impl, setImpl] = useState(null);
+
+    useEffect(() => {
+        csvLoader("tags", (table) => setTags(table));
+        csvLoader("related", (table) => setRelated(table), true);
+        csvLoader("group", (table) => setGroup(table), true);
+        csvLoader("impl", (table) => setImpl(table));
+    }, []);
+
     return <></>;
 }
