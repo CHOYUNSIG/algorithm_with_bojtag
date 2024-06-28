8e09ed985d9c3270a9717d12dc9b696a54b15065
조윤식 <cho.yunsig56@gmail.com>
2024-06-29 02:42:49 +0900

    홈 화면 수정

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { maxContent } from "../constants";
+import { Link } from "react-router-dom";
+import { brandColor } from "../constants";
 
 const HomeWrapper = styled.div`
   display: flex;
@@ -11,6 +13,40 @@ const HomeWrapper = styled.div`
     box-sizing: border-box;
     padding: 16px;
     text-align: center;
+  }
+`;
+
+const Button = styled.button`
+  margin: 5px;
+  height: fit-content;
+  padding: 10px;
+  background-color: transparent;
+  border: none;
+  border-radius: 16px;
+  box-shadow: 2px 2px 10px #aaaaaa;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+
+  * {
+    font-size: 1.3em;
+    transition: 0.2s;
+  }
+
+  &:hover {
+    * {
+      font-size: 1.6em;
+    }
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
   }
 `;
 
@@ -31,6 +67,24 @@ export default function Home() {
           이 블로그는 문제 해결에 쓰이는 알고리즘을 백준 태그를 중심으로 정리한
           블로그입니다.
         </p>
+        <div
+          style={{
+            padding: "16px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: "50px",
+          }}
+        >
+          <Button>
+            <i className="fa fa-link"></i>
+            <Link to="/tags">Tags</Link>
+          </Button>
+          <Button>
+            <i className="fa fa-link"></i>
+            <Link to="/post">Posts</Link>
+          </Button>
+        </div>
       </div>
     </HomeWrapper>
   );
